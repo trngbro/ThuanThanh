c15882c7a4f35786cc8fe72f1b984bece88fdc20
trngxbro <trngxbro@hotmail.com>
2022-06-02 09:48:42 +0700

    clean_warming_Database

--- app/src/main/java/com/tdtu/thuanthanh/Database/CreateDatabase.java
+++ app/src/main/java/com/tdtu/thuanthanh/Database/CreateDatabase.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
 public class CreateDatabase extends SQLiteOpenHelper {
 
     public static String TBL_NHANVIEN = "NHANVIEN";
